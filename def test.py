39d4d8e3278aa19166941477b073eaad186e08af
jinyaodesandies <60386838+jinyaodesandies@users.noreply.github.com>
2020-06-09 11:17:58 +0800

    Add files via upload

---
+++ def test.py
@@ -0,0 +1,10 @@
+def printme(x):
+    print(x)
+printme("one")
+def inputme(x,y,z):
+    print(x)
+    print(y)
+    input(z)
+inputme("you are tired so you fall asleep]\n","you wake up and you fell uncomfortable\n","you can choice what to do.")
+    
+    
